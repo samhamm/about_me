846fa17d37f421e5c147134c306db0abcda46bf9
Sam Hamm <sam@codefellows.com>
2018-05-01 13:46:01 -0700

    Greeting works

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 'use strict';
 
-alert('IT IS ALIVE');
+// Ask for user's name
+var userName = prompt('Hi! What is your name!');
+
+// Greet the user by name in an alert
+alert('Hi, ' + userName + ', thanks for stopping by!');
+
+// Log that to the console
+console.log('The user said their name is ' + userName);
+
+// Ask the first guessing game question
+// User guesses "yes/no" or "Y/N"
+// Tell user if they are wrong or right
+
+// Next question
